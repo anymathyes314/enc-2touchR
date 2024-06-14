c988fe7097a232127943d99092902e214bd86a86
anymathyes <166995325+anymathyes314@users.noreply.github.com>
2024-06-14 21:11:52 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 function encryption() {
     const char = 'ABC';
     for (let i = 0; i < char.length; i++) {
-        const ascii = ascii + char.charCodeAt(i);
+        const ascii = char.charCodeAt(i);
     }  
     console.log(ascii); // 出力: 65
 }
